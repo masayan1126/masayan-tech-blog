65e721c22497dc969864840eb20adfb7ee5636e2
nishigaki <nishigaki@aivick.co.jp>
2023-06-27 17:39:10 +0900

    fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  },
   theme: {
     extend: {},
   },
