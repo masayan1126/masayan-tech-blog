26f99df1fe996b1ff86316df4a1262dad1a2f430
masayan1126 <masa199311266@gmail.com>
2023-07-16 19:22:15 +0900

    Add buildPaginationItem function to generate pagination items

---
+++ src/features/Pagination/buildPaginationItem.ts
@@ -0,0 +1,30 @@
+/**
+ *
+ * @param total 総記事数
+ * @param size 1ぺージあたりの記事数
+ * @param currentPage 現在のページ
+ * @returns ページネーションのアイテムの配列
+ */
+export default function (
+  total: number,
+  size: number,
+  currentPage: number
+): string[] {
+  const numListOfPagination = [...Array(Math.ceil(total / size))].map(
+    (_, i) => i + 1
+  );
+
+  const lastPage = numListOfPagination[numListOfPagination.length - 1];
+  return numListOfPagination.map((num) => {
+    // 最初と最後のページは必ず表示する
+    if (num === 1 || num === lastPage) {
+      return String(num);
+    }
+
+    const diff = Math.abs(currentPage - num);
+    if (diff > 3) return "";
+
+    // 現在のページから前後3ページ以内は表示する
+    return String(num);
+  });
+}
---
+++ tests/buildPagenationItem.test.ts
@@ -0,0 +1,26 @@
+import buildPaginationItem from "@/features/Pagination/buildPaginationItem";
+
+describe("ページネーションのアイテム生成関数", () => {
+  describe("ページネーションのアイテムを配列として生成できる", () => {
+    test("全104記事で1ページあたり10記事を表示、現在のページが3ページの場合は1から5ページまでと最後の11ページが表示", () => {
+      const actual = buildPaginationItem(104, 10, 3);
+      expect(actual).toStrictEqual([
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "",
+        "",
+        "",
+        "",
+        "11",
+      ]);
+    });
+    test("全記事が1ページに収まる場合は1ページのみ表示", () => {
+      const actual = buildPaginationItem(9, 10, 1);
+      expect(actual).toStrictEqual(["1"]);
+    });
+  });
+});
