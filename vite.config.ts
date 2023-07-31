85fe56771f2ae834f0277acc65823bf0014cf039
masayan1126 <masa199311266@gmail.com>
2023-08-01 07:49:51 +0900

    Add Jest DOM to test setup and configure jsdom environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from "path";
 export default defineConfig({
   test: {
     globals: true,
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
     reporters: ["default", "html"],
     coverage: {
       provider: "c8", // カバレッジ
