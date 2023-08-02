205188d7ce574672120eb39f6028d3dd0925a1b6
masayan1126 <masa199311266@gmail.com>
2023-08-03 07:34:40 +0900

    Fix env

--- src/libs/microcms/config.ts
+++ src/libs/microcms/config.ts
@@ -1,5 +1,5 @@
 import { createClient } from "microcms-js-sdk";
 export const client = createClient({
-  serviceDomain: import.meta.env.VITE_MICRO_CMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.VITE_MICRO_CMS_API_KEY,
+  serviceDomain: import.meta.env.MICRO_CMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICRO_CMS_API_KEY,
 });
