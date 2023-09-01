7d8288eedde0f6ec517450e0df19eb03773ee985
masayan1126 <masa199311266@gmail.com>
2023-09-02 08:34:22 +0900

    Upate astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-
-import image from "@astrojs/image";
 import { SITE_DOMAIN } from "./src/consts"; //FIXME: mjs alias(参考：https://stackoverflow.com/questions/70158055/how-to-have-aliases-with-nodejs-import)
 
 // https://astro.build/config
@@ -21,7 +19,6 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
-    image(),
   ],
   server: {
     port: 53000,
