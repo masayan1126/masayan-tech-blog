74dbbe19a20f043703b48f749588343360b11258
masayan1126 <masa199311266@gmail.com>
2023-09-02 08:34:32 +0900

    Fix other

--- src/features/Header/headerLinkMapper.ts
+++ src/features/Header/headerLinkMapper.ts
@@ -10,15 +10,15 @@ headerLinkMapper
   ])
 
   .set("blog", [
-    { href: "/", text: "Home", iconName: "carbon:home" },
+    { href: "/page/1", text: "Home", iconName: "carbon:home" },
     { href: "/about", text: "About", iconName: "carbon:user-profile-alt" },
   ])
   .set("about", [
-    { href: "/", text: "Home", iconName: "carbon:home" },
+    { href: "/page/1", text: "Home", iconName: "carbon:home" },
     // { href: "/blogs", text: "Blog", iconName: "jam:blogger-square" },
   ])
   .set("disclaimer", [
-    { href: "/", text: "Home", iconName: "carbon:home" },
+    { href: "/page/1", text: "Home", iconName: "carbon:home" },
     { href: "/about", text: "About", iconName: "carbon:user-profile-alt" },
   ]);
 
