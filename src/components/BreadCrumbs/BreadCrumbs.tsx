82096e467b9cf6b97fd1b06b049e3e87a2683ac3
masayan1126 <masa199311266@gmail.com>
2023-09-02 12:43:13 +0900

    Add bread crumbs to blog page

---
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -0,0 +1,36 @@
+type Props = {
+  lists: {
+    name: string;
+    path: string;
+  }[];
+};
+
+export const BreadCrumbs = ({ lists }: Props) => {
+  if (!lists) {
+    return "";
+  }
+
+  return (
+    <ol
+      className="flex font-bold overflow-x-auto whitespace-nowrap text-xs mb-8"
+      aria-label="breadcrumb"
+    >
+      {lists.map(({ name, path }, index) => (
+        <li className="flex gap-6 items-center" key={index}>
+          {lists.length - 1 !== index ? (
+            <>
+              <a className="text-sm md:text-base underline" href={path}>
+                {name}
+              </a>
+              <span className="mr-3">{">"}</span>
+            </>
+          ) : (
+            <span className="text-sm md:text-base" aria-current="page">
+              {name}
+            </span>
+          )}
+        </li>
+      ))}
+    </ol>
+  );
+};
