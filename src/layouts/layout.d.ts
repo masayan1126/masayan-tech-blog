2b870a5b73708f8e088a5f1226457f7a82ac07b0
masayan1126 <masa199311266@gmail.com>
2024-01-08 09:34:19 +0900

    Update dependencies and remove commented code

--- src/layouts/layout.d.ts
+++ src/layouts/layout.d.ts
@@ -1,14 +1,3 @@
-// export type BaseLayoutProps = {
-//   title: string;
-//   description: string;
-//   context: string;
-//   heroImage?: string;
-//   breadCrumbsList: {
-//     name: string;
-//     path: string;
-//   }[];
-// };
-
 export interface BasicLayoutProps {
   title: string;
   description: string;
